55ebcad0c9fa6c61c25bd517e434905289234449
JosefAlbers <146810011+JosefAlbers@users.noreply.github.com>
2024-07-28 14:42:57 +0900

    posts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="rd-to-md",
     url='https://github.com/JosefAlbers/rd2md',
-    version="0.0.2",
+    version="0.0.3",
     py_modules=["rd2md"],
     packages=find_packages(),
     readme="README.md",
